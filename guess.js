65fd38f14d03e2b2ffd6be5cb47bace96e288110
vtisza <vtisza@gmail.com>
2020-10-10 23:44:09 +0200

    add additional js files

---
+++ guess.js
@@ -0,0 +1,19 @@
+import { displayImage } from './random_image';
+import { write_guess } from './store';
+
+ export function guess()  {
+    var chili_input = document.getElementById("ChiliInput");
+    var solution = document.getElementById("solution");
+    var chili_picture = document.getElementById("chili-picture");
+    total_guess++;
+    if (chili_input.value.split(" ").join("_")==chili_picture.src.split("/")[6]) {
+        good_guess++;
+        solution.innerHTML = "Correct";
+      } else {
+        solution.innerHTML = "Nope it was:" + chili_picture.src.split("/")[6].split("_").join(" ");
+      }
+    solution.innerHTML += "<br> Result: "+good_guess+"/"+total_guess;
+    write_guess();
+    displayImage();
+
+  }
